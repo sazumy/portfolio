fc5f201b54c4c28c5ad86e3efc18023a00cdd0ac
okutamatanuki <seinoazumi@gmail.com>
2019-06-23 00:18:51 +0900

    Setting JS

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,33 @@
 $(document).ready(function(){
-  
-  $("#scroll-top-btn").on('click', function(){
-    console.log('click');
+  $(".scroll-top-btn").each(function(){
+
+    var $el = $(scrollableElement('html', 'body'));
+
+    $(".scroll-top-btn").on('click', function(e){
+      e.preventDefault();
+      $($el).animate({ scrollTop: 0}, 300);
+    });
+
+    // scrollTop が利用できる要素を検出する関数
+    // http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links#update4
+    function scrollableElement (elements) {
+      var i, len, el, $el, scrollable;
+      for (i = 0, len = arguments.length; i < len; i++) {
+        el = arguments[i],
+        $el = $(el);
+        if ($el.scrollTop() > 0) {
+          return el;
+        } else {
+          $el.scrollTop(1);
+          scrollable = $el.scrollTop() > 0;
+          $el.scrollTop(0);
+          if (scrollable) {
+            return el;
+          }
+        }
+      }
+      return [];
+    }
+
   });
-  
 });
--- js/import.js
+++ js/import.js
@@ -1,3 +1,4 @@
 document.write('<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>');
 document.write('<script src="https://kit.fontawesome.com/1f22ee88f6.js"></script>');
 document.write('<script src="js/animation.js"></script>');
+document.write('<script src="js/slider.js"></script>');
