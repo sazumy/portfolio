934ec06cadcbc5eac8632353258e83f337a30a2e
okutamatanuki <seinoazumi@gmail.com>
2019-06-23 21:46:46 +0900

    add mordal window

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-
+// ギャラリーを表示する関数 -----------------------------
   $('#gallery').each(function(){
     
     var $container = $(this);
 
-// masonryの初期値を設定 -----------------------------
+    // masonryの初期値を設定 -----------------------------
     $container.masonry({
       columnWidth: 270,
       gutter: 10,
@@ -18,11 +18,17 @@ $(document).ready(function(){
       $.each(data, function (i, item) {
 
         var itemHTML =
-          '<li class="gallery-item is-loading">' +
-            '<a href="' + item.images.large + '">' +
+          '<li class="gallery-item is-loading" id="' + item.id + '">' +
               '<img src="' + item.images.thumb +
-                  '" alt="' + item.title + '">' +
-            '</a>' +
+                  '" class="hidden_in_mordal"' +
+                  'alt="' + item.title + '">' +
+              '<div class="hidden">' + 
+                '<img src="' + item.images.large + '" alt="' + item.title + '">' +
+                '<div class="right__content">' +
+                '<h3 class="works__title">' + item.title + '</h3>' +
+                '<p>' + item.description + '</p>'
+                '</div>'
+              '</div>'
           '</li>';
 
         elements.push($(itemHTML).get(0));
@@ -35,6 +41,34 @@ $(document).ready(function(){
         $(elements).removeClass('is-loading');
         $container.masonry('appended', elements);
       });
+    });
+  });
+
+// クリックされた画像をモーダルで表示する関数---------------------------
+  
+  $('#gallery').on('click', 'li.gallery-item', function(){
+
+    var mordalHTML = $(this).html(),
+      $mordal = $('#mordal');
+      $mordalInner = $('#mordal__inner'),
+      $openInMordal = $(this).find('div.hidden'),
+      $worksInfo = $('.works__info');
+
+    $mordal.css({"display": "block"});
+    $worksInfo.empty();
+    $worksInfo.append(mordalHTML);
+    $mordalInner.css({"display": "block"});
+
+    if ($(this).closest('.mordal__inner').length > 0){
+    $openInMordal.css({"display": "block"});
+    }
+  });
+
+  $(document).ready(function(){
+   
+    $('.mordal__close').on('click', function(){
+      $('#mordal').css({"display": "none"});
+      $('#mordal__inner').css({"display": "none"});
     });
   });
 });
