757723fc552d4c944b6604dec19bdacb133a4e90
aocattleya <aocattleya@yahoo.co.jp>
2021-02-08 00:11:16 +0900

    add: スライドをマウス移動でページャー発火させる機能の追加

--- js/slider.js
+++ js/slider.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
   $slideGroup = $container.find('.slideshow-slides'),
   $slides = $slideGroup.find('.slide'),
   $nav = $container.find('.slideshow-nav'),
- 
+  isNavNext = true,
+  isNavPrev = false,
+  mousePosition = 0,
+
   slideCount = $slides.length,
   indicatorHtml = '',
   currentIndex = 0,
@@ -26,7 +29,7 @@ $(document).ready(function(){
       currentIndex = index;
       updateNav();
     }
-  
+
   // ナビゲーターの値を更新する関数----------------------------//
 
     function updateNav (){
@@ -35,14 +38,18 @@ $(document).ready(function(){
 
       if(currentIndex === 0){
         $navPrev.addClass('disabled');
+        isNavPrev = false;
       } else {
         $navPrev.removeClass('disabled');
+        isNavPrev = true;
       };
 
       if(currentIndex === slideCount -1){
         $navNext.addClass('disabled');
+        isNavNext = false;
       } else {
         $navNext.removeClass('disabled');
+        isNavNext = true;
       };
     }
 
@@ -55,6 +62,44 @@ $(document).ready(function(){
         goToSlide(currentIndex+ 1);
       }
     });
+
+  // ドラッグ移動でページ送り-----------------//
+  $slideGroup.mousedown(function (e) {
+    e.preventDefault();
+    // 0のクリック初期位置
+    mousePosition = getPosition(e);
+
+    $slideGroup.off('mousemove mouseup');
+    $slideGroup.on('mousemove mouseup', function handler(e) {
+      if (e.type === 'mousemove') {
+        // 右に30以上マウス移動していた場合
+        if (mousePosition - getPosition(e) > 30 && isNavNext) {
+          goToSlide(currentIndex + 1);
+          $slideGroup.off('mousemove');
+          // クリック発火させない（今後クリックで何かする場合用）
+          $slideGroup.click(function(e) {
+              e.preventDefault();
+          });
+        // 左に30以上マウス移動していた場合
+        } else if (mousePosition - getPosition(e) < -30 && isNavPrev) {
+          goToSlide(currentIndex - 1);
+          $slideGroup.off('mousemove');
+          // クリック発火させない（今後クリックで何かする場合用）
+          $slideGroup.click(function(e) {
+              e.preventDefault();
+          });
+        }
+      // マウスクリックを外した時かつ、左右30以上動いていなかったら何もしない
+      } else if (e.type === 'mouseup' && (mousePosition - getPosition(e) > -30) && (mousePosition - getPosition(e) < 30)) {
+        $slideGroup.off('mousemove mouseup');
+      }
+    })
+  });
+
+  // マウス位置取得-----------------//
+  var getPosition = function(e) {
+      return e.originalEvent.pageX;
+  }
 
   // スライダーを動作させる-----------------//
     goToSlide(currentIndex);
